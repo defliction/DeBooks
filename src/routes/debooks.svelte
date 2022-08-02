f3c26eea0493bb443ea09ba89c258eb86cba273b
revick <53401421+revick@users.noreply.github.com>
2022-08-02 21:17:44 +0200

    no records for period display

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -621,6 +621,20 @@ $: end, $currentPage = 1
 </div>
 
 {/if}
+{#if validKey == true && !loading && $fetchedTransactions.length == 0}
+    <div class="flex justify-center flex-row">
+        <div class="pt-10">
+            <div class="alert shadow-lg font-serif">
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <span>No records for this period.</span>
+                </div>
+            </div>
+        
+        </div>
+
+    </div>
+{/if}
 <style>
 .custom-pagination-nav :global(.pagination-nav) {
   display: flex;
