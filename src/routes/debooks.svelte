2e37f3fa865a81bc5c46436098dfd8f3d84ee6d0
revick <53401421+revick@users.noreply.github.com>
2022-08-04 14:30:39 +0200

    table flexes correctly

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -35,7 +35,7 @@
     let loading = false;
 
     //let start = new Date(2022,6,1)
-    let start = dayjs().subtract(7, 'days').format("YYYY-MM-DD")
+    let start = dayjs().subtract(1, 'days').format("YYYY-MM-DD")
     $: startday = dayjs(start).startOf('day')
     //let end = new Date(2022,6,6)
     let end = dayjs().format("YYYY-MM-DD")
@@ -680,7 +680,7 @@ $: $showfees, sliceDisplayArray(), !$showfees? $currentPage > totalPages? $curre
 $: $displayArray, sortArray($displayArray)
 $: $textFilter, sliceDisplayArray(), $currentPage = 1
 $: currentTransaction != 0? currentPercentage = "" + Math.round(currentTransaction/$fetchedTransactions.length*100) + "%" : ""
-$: condition = innerWidth < 640
+$: condition = innerWidth < 755
 $: start,$currentPage = 1
 $: end, $currentPage = 1 
 //$: (async() => $keyInput = await checkKey ())();
@@ -761,7 +761,7 @@ $: end, $currentPage = 1
             {#if !loading } 
                     <div class="col-start-auto">
                         
-                        <input type="text" placeholder="Search: e.g. Magic Eden..." bind:value={$textFilter} class="input input-xs sm:min-w-[28rem] sm:max-w-[28rem] " />
+                        <input type="text" placeholder="Search: e.g. Magic Eden..." bind:value={$textFilter} class="input input-xs  max-w-[28rem] " />
                     </div>
                     <div class="col-end-auto grid grid-flow-col">
                         <div class="grid grid-flow-col">
@@ -822,14 +822,14 @@ $: end, $currentPage = 1
                     
         </div>
                 
-        <table class="table table-compact normal-case ">
+        <table class="flex table table-compact normal-case ">
             
           <!-- head -->
           <thead>
             <tr class=" ">
               
                 <th class="min-w-[2rem] text-left normal-case">Date</th>
-                <th class="sm:min-w-[28rem] sm:max-w-[28rem] min-w-[12rem] max-w-[12rem] text-left normal-case">Description</th>
+                <th class="min-w-[12rem] max-w-[28rem] text-left normal-case">Description</th>
                 {#if !condition}
                     <th class="min-w-[4rem] text-left normal-case">Sig</th>
                 {/if}
@@ -849,16 +849,16 @@ $: end, $currentPage = 1
                 <tr class="">
                     
                     <td class="min-w-[2rem] text-left">{dayjs.unix(transaction.timestamp).format('YYYY-MM-DD')}</td>
-                    <td class="whitespace-normal sm:min-w-[28rem] sm:max-w-[28rem] min-w-[12rem] max-w-[12rem] text-left">{transaction.description}</td>
+                    <td class="whitespace-normal min-w-[12rem] max-w-[28rem] text-left">{transaction.description}</td>
                     {#if !condition}
                         <td class="min-w-[4rem] text-left">{transaction.signature.substring(0,4)}...</td>
                     {/if}
                     <td class="min-w-[2rem] text-right">{transaction.amount?.toLocaleString('en-US', { maximumFractionDigits: 10 })}</td>
                     {#if showConversion}
                         {#if convertingToReporting} 
-                            <td class="min-w-[2rem] text-right"><progress class="progress w-[2rem]"></progress></td>
+                            <td class="min-w-[4rem] text-right"><progress class="progress w-[2rem]"></progress></td>
                         {:else}
-                            <td class="min-w-[2rem] text-right">{transaction.usd_amount?.toLocaleString('en-US', { minimumFractionDigits: 2, trailingZeroDisplay :true, maximumFractionDigits: 4 })}</td>
+                            <td class="min-w-[4rem] text-right">{transaction.usd_amount?.toLocaleString('en-US', { minimumFractionDigits: 2, trailingZeroDisplay :true, maximumFractionDigits: 4 })}</td>
                         {/if}
                         
                     {/if}
@@ -893,7 +893,7 @@ $: end, $currentPage = 1
             <footer class=" footer px-10 py-4 border-t rounded-md bg-base-200 text-base-content border-base-300 ">
                 <div class="items-center grid-flow-col justify-self-start">
                   <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-                  <p>Copyright © 2022 - DeBooks.xyz</p>
+                  <p>Copyright © 2022 - DeBooks</p>
                 </div> 
                 <div class="md:place-self-center md:justify-self-end justify-self-center">
                   <div class="grid grid-flow-col gap-4">
