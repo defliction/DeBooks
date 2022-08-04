c16b01a74d3e9f1dcafa57cb07e5aae3b2b52d79
revick <53401421+revick@users.noreply.github.com>
2022-08-04 15:11:45 +0200

    tweaks to table sizes

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -834,9 +834,9 @@ $: end, $currentPage = 1
                     <th class="min-w-[4rem] text-left normal-case">Sig</th>
                 {/if}
                 
-                <th class="min-w-[4rem] max-w-[4rem] text-right normal-case">Base</th>
+                <th class="min-w-[4rem] max-w-[8rem] text-right normal-case">Base</th>
                 {#if showConversion}
-                <th class="min-w-[4rem] max-w-[4rem] text-right normal-case">{$reportingCurrency}</th>
+                <th class="min-w-[4rem] max-w-[6rem] text-right normal-case">{$reportingCurrency}</th>
                 {/if}
                 <th class="min-w-[2rem]"></th>
             </tr>
@@ -853,12 +853,12 @@ $: end, $currentPage = 1
                     {#if !condition}
                         <td class="min-w-[4rem] text-left">{transaction.signature.substring(0,4)}...</td>
                     {/if}
-                    <td class="min-w-[2rem] text-right">{transaction.amount?.toLocaleString('en-US', { maximumFractionDigits: 10 })}</td>
+                    <td class="min-w-[2rem] max-w-[8rem] text-right">{transaction.amount?.toLocaleString('en-US', { maximumFractionDigits: 10 })}</td>
                     {#if showConversion}
                         {#if convertingToReporting} 
-                            <td class="min-w-[4rem] text-right"><progress class="progress w-[2rem]"></progress></td>
+                            <td class="min-w-[4rem] max-w-[6rem] text-right"><progress class="progress w-[2rem]"></progress></td>
                         {:else}
-                            <td class="min-w-[4rem] text-right">{transaction.usd_amount?.toLocaleString('en-US', { minimumFractionDigits: 2, trailingZeroDisplay :true, maximumFractionDigits: 4 })}</td>
+                            <td class="min-w-[4rem] max-w-[6rem]  text-right">{transaction.usd_amount?.toLocaleString('en-US', { minimumFractionDigits: 2, trailingZeroDisplay :true, maximumFractionDigits: 4 })}</td>
                         {/if}
                         
                     {/if}
@@ -893,9 +893,9 @@ $: end, $currentPage = 1
             <footer class="footer footer-center p-4 bg-base-200 text-base-content rounded-md">
                 
                 <div class="items-center grid-flow-col">
-                    <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
+                    <a href="https://twitter.com/defliction"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg>
                     </a>
-                  <p>Copyright © 2022 DeBooks.xyz</p>
+                  <p>DeBooks © 2022</p>
                 </div>
             </footer>
         </div>
