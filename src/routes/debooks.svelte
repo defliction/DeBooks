28913f821b4b7280c1ed62b0dd0e2a6cdf8ed339
revick <53401421+revick@users.noreply.github.com>
2022-08-05 14:52:39 +0200

    toggle metadata button

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -712,8 +712,48 @@ $: !validKey? $currentPage = 1 : $currentPage=$currentPage
 
 <div class="flex justify-center">
     
-    <div class="pt-4 text-center ">
- 
+    
+    <div class="pt-2 text-center ">
+        <div class="grid grid-flow-col place-items-end z-50">
+       
+                    <div class="tooltip tooltip-bottom" data-tip="Toggle token metadata off/on (loading is faster without fetching NFT names etc..)">
+                        {#if loading}
+                        <button on:click={() => {$showMetadata = !$showMetadata}} disabled class="btn btn-xs btn-ghost normal-case ">
+                            {#if $showMetadata}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-current fill-transparent" viewBox="0 0 24 24" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                </svg>
+                                {:else}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-transparent fill-primary" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd" />
+                                </svg>
+                            {/if}
+                            
+                        </button>
+                        {:else}
+                            <button on:click={() => {$showMetadata = !$showMetadata}} class="btn btn-xs btn-ghost normal-case ">
+                                {#if $showMetadata}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-current fill-transparent" viewBox="0 0 24 24" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                                </svg>
+                                {:else}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 stroke-transparent fill-primary" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd" />
+                                </svg>
+                                {/if}
+                                
+                            </button>
+                        {/if}
+                        
+                        
+                    </div>
+        
+            
+           
+               
+        
+         
+        </div>
             
         <h1 class="pb-2 font-rosu1 text-5xl text-center">DeBooks</h1>
         
--- src/utils/priceFetcher.py
+++ src/utils/priceFetcher.py
@@ -4,3 +4,22 @@ import numpy as np
 import pandas as pd
 import multiprocessing
 import os
+
+file_path = "log_file.txt"
+pairs = ["warpped-solana", "USDC", "mSOL"]
+
+
+def main(): 
+    while True: 
+        try:
+            print("XYZ")
+        except KeyboardInterrupt:
+            print('interrupted!', file=open(file_path, "a"))
+            break
+        except Exception as e:
+            print("XY2Z")
+              
+if __name__ == "__main__":
+        main()
+
+
