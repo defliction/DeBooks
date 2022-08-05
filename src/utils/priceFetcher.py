9e524c3eea6bba4b97870e356aab5762919fb031
revick <53401421+revick@users.noreply.github.com>
2022-08-05 21:54:40 +0200

    same day fix

--- src/utils/priceFetcher.py
+++ src/utils/priceFetcher.py
@@ -22,23 +22,30 @@ def main():
             df.date = pd.to_datetime(df.date, format='%d-%m-%Y')
             #print(df)
             latest_date = df.date.max()
-            next_date = latest_date + datetime.timedelta(days=1)
+            next_date = latest_date + datetime.timedelta(days=0)
             today = datetime.date.today()
             #print(latest_date.strftime("%d-%m-%Y"))
             #print(next_date.strftime("%d-%m-%Y"))
             #print((next_date - latest_date).days)
             #print(today.day)
-            
-            while  pd.Timestamp(today)  >=  pd.Timestamp(next_date) :
+            print("today ", pd.Timestamp(today), "next ", pd.Timestamp(next_date))
+            while  pd.Timestamp(today).date() >=  pd.Timestamp(next_date).date() :
                 response = requests.get("https://pro-api.coingecko.com/api/v3/coins/wrapped-solana/history?date="+next_date.strftime("%d-%m-%Y") + "&x_cg_pro_api_key=CG-F3PXm3JzJRLx48C6cvfMvvrk")
                 json_data = json.loads(response.text)
-                print(datetime.datetime.now(), "fetched ", json_data['market_data']['current_price']['usd'])
+                print(datetime.datetime.now(), "fetched ",next_date.strftime("%d-%m-%Y"), json_data['market_data']['current_price']['usd'])
                 
                 df.loc[-1] = [json_data['id'], next_date, json_data['market_data']['current_price']['usd']]  # adding a row
+                
+                if pd.Timestamp(today).date() ==  pd.Timestamp(next_date).date():
+                    print("same dates")
+                    df = df.iloc[1: , :]
+
+                next_date = next_date + datetime.timedelta(days=1)
                 df.index = df.index + 1  # shifting index
                 df = df.sort_index()  # sorting by index
                 
-                next_date = next_date + datetime.timedelta(days=1)
+                
+                    
                                
                 
             #print(df)
@@ -46,7 +53,7 @@ def main():
             #print(df)
             
             df.to_json('wrapped-solana.json', orient='records')
-            print(datetime.datetime.now(), "updated json data, latest date:", df.date[0])
+            print(datetime.datetime.now(), "updated json data, latest date/price", df.date[0], df.usd[0])
             time.sleep(3600)
 
 
