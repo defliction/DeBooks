8324f49c964a2154a2158bee903cb692c103e896
revick <53401421+revick@users.noreply.github.com>
2022-08-23 15:16:25 +0200

    tweak

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -112,7 +112,7 @@
         //console.log("latest date ", storedCoinGeckoData[0])
         //fetch historics manually
         
-        console.log(rpcConnection)
+        //console.log(rpcConnection)
         //await interpolateBlockSignatures()
         console.log("END - starting logs")
     });
@@ -1086,7 +1086,7 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-primary-focus" fill="none" viewBox="0 0 24 24"  stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
                       </svg>
-                        <span>Establishing connection to RPC network...</span>
+                        <span>Establishing connection to network...</span>
                 {/if}
             </div>
         </div>
