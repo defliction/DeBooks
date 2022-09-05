7f1f1fa72653b412b713faeb516ca38a59538430
revick <53401421+revick@users.noreply.github.com>
2022-09-05 22:56:32 +0200

    remove image icon

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -90,8 +90,8 @@
         }
         //console.log("first date", firstDate)
         //first blocktimed block - 38669748
-        let trans1 = await $cnx.getParsedTransaction("ktxAdyQU7dzAnJy7RboBbDfSsuF6uFGdEmvU5SwLtndDzEahcegZnWc7ymLLUNoQgdV1QugTrMUqDVyvY1f7vv6")
-        console.log(trans1)
+        //let trans1 = await $cnx.getParsedTransaction("ktxAdyQU7dzAnJy7RboBbDfSsuF6uFGdEmvU5SwLtndDzEahcegZnWc7ymLLUNoQgdV1QugTrMUqDVyvY1f7vv6")
+        //console.log(trans1)
         //let block = 38669748
         //let startB = await $cnx.getBlockTime(block)
         //console.log("b1 ",startB, dayjs.unix(startB).format("DD-MM-YYYY"), startday.format("DD-MM-YYYY"), (dayjs.unix(startB).diff(startday, 'hours')), block)
@@ -1120,9 +1120,7 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
                             <Popover action="hover" arrow={false} overlayColor=rgba(0,0,0,0) >    
                                 <div slot="target" class="flex justify-center align-middle flex-row ">
                                    <span class='pr-2'> {transaction.description}</span>
-                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                                  </svg>
+                                  
                                   
                                 </div>
                                 
