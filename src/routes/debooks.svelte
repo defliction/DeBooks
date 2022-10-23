f9b2e3cdd021330be13f8e395fef9870fc612f44
revick <53401421+revick@users.noreply.github.com>
2022-10-23 16:22:25 +0200

    test
    da

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -68,7 +68,7 @@
     let showInfoTip = false
     let invalidKey = false;
 
-
+    //https://solana-mainnet.g.alchemy.com/v2/AtE9_yJOMYOrEYcu5EpkPPvEv-jVKafC
     //const connection = new web3.Connection("https://ssc-dao.genesysgo.net");
     $cnx = new web3.Connection("https://solitary-young-butterfly.solana-mainnet.discover.quiknode.pro/73898ef123ae4439f244d362030abcda8b8aa1e9/");
     //const metap = new Metaplex($connection)
