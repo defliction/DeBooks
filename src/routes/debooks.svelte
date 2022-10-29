11b7139f069487626f55cb13442e883c46c1c991
revick <53401421+revick@users.noreply.github.com>
2022-10-29 16:15:21 +0200

    small change

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -1017,12 +1017,14 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
         {#if loading == false && rpcConnection == true}
         
         <div class="input-group justify-center">
+        
             <input type="text" placeholder="enter account address e.g. DeDao..uw2r" on:keydown={onKeyDown} bind:value={$keyInput} class=" text-center font-serif input input-sm input-bordered input-primary sm:w-96 w-64 " />
             {#if $keyInput != ""}
             <button class="btn btn-primary btn-sm btn-square" on:click={checkKey}>
                 GO
                 </button>
             {:else}
+            
             <button disabled class="btn btn-sm btn-square">
                 GO
                 </button>
@@ -1293,17 +1295,11 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
         <div class="pt-10">
             <div class="alert shadow-lg font-serif">
                 <div> 
-                   
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-primary-focus flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                   
-                        <span>We're working on it - fetching metadata, particularly lots of NFTs data can feel slow</span>
-
-                        
+                        <span>We're working on it - fetching metadata, particularly lots of NFTs data can feel slow</span>      
                 </div>
             </div>
-        
         </div>
-    
     </div>
     {/if}
     {#if !loading && $fetchedTransactions.length == 0}
@@ -1374,7 +1370,8 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
         <em class="text-sm pb-2">last updated: October 2022</em>
         <h4>Feature priority roadmap:</h4>
         <ul class="list-disc leading-4">
-            <li>Richer AMM and DeFi integrations</li> 
+            <li>Classification engine V2</li> 
+            <li>(Ongoing) Wider protocol integrations</li> 
             <li>xNFT integration</li> 
             <li>Solana Mobile Stack / Saga native app</li>
             <li>Support for multiple wallets</li>
