8a5a8575fc149fdf6008990b6bdf6b51c96b344e
revick <53401421+revick@users.noreply.github.com>
2022-10-30 19:25:37 +0200

    t

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -1204,32 +1204,3 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
 </svelte:head>
 
 
-<style>
-.custom-pagination-nav :global(.pagination-nav) {
-  display: flex;
-  justify-content: flex-end;
-  border-radius: 3px;
-  box-shadow: 0 1px 2px hsl(var(--b1));
-}
-.custom-pagination-nav :global(.option) {
-  padding: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  transition: 0.2s all ease-out;
-  user-select: none;
-  color: hsl(var(--n));
-}
-
-.custom-pagination-navv :global(.option.number),
-.custom-pagination-nav :global(.option.ellipsis) {
-  padding: 5px 7px;
-}
-.custom-pagination-nav :global(.option:hover) {
-  background: hsl(var(--b2));
-  cursor: pointer;
-}
-.custom-pagination-nav :global(.option.active) {
-  color: hsl(var(--pf));
-}
-</style>
