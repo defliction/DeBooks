039c84482fd45c47660043a8060d1b187703251f
revick <53401421+revick@users.noreply.github.com>
2022-10-31 21:22:25 +0200

    wallet butotn

--- src/routes/wallets.svelte
+++ src/routes/wallets.svelte
@@ -6,15 +6,22 @@
 
 <div class=" py-4">
     {#if $keyList.length > 0 }
-    <table class="table table-compact normal-case ">
-        <thead>
+    <table class="table table-compact normal-case">
+        <thead on:click={()=> expand = !expand} >
             <tr class=" ">
-                <th class="min-w-[22rem] text-left normal-case">Wallet addresses ({$keyList.length})</th>
-                <th class="min-w-[4rem] text-left normal-case">Status</th>
+                <th class="min-w-[22rem] text-left text-sm normal-case">Wallet addresses ({$keyList.length})</th>
+                <th class="min-w-[4rem] text-left text-sm normal-case">Status</th>
                 {#if expand}
-                    <th class="btn btn-sm" on:click={()=> expand = !expand}>+</th>
+                <th><button class="btn btn-ghost btn-xs" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-4 h-4 stroke-primary">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                  </svg>
+                </button></th>
+                    
                 {:else}
-                    <th class="btn btn-sm" on:click={()=> expand = !expand}>-</th>
+                <th><button class="btn btn-ghost btn-xs" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-4 h-4 stroke-primary">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                  </svg>
+                </button></th>
                 {/if}
             </tr>
         </thead>
@@ -24,7 +31,11 @@
             <tr class="">
                 <td class=" min-w-[12rem] text-left text-xs">{item}</td>
                 <td class=" min-w-[4rem] text-left text-xs">ready</td>
-                <td class="btn btn-sm min-w-[2rem] text-left text-xs" on:click={()=> ($keyList.splice(i,1), $keyList=$keyList)}>x</td>
+                <td><button class="btn btn-ghost btn-xs min-w-[2rem]" on:click={()=> ($keyList.splice(i,1), $keyList=$keyList)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-4 h-4 stroke-primary">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </button></td>
             </tr>
             {/each}
         </tbody>
