e459955d59a0582bc1d881475d91537394addb25
revick <53401421+revick@users.noreply.github.com>
2022-11-03 21:32:46 +0200

    splice slice fix

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -323,7 +323,15 @@
         }   
         
     }
+    async function removeKeyHandler(i) {
 
+        $keyList.splice(i,1) 
+        $keyList=$keyList
+        $fullArray.splice(i,1)
+        sliceDisplayArray()
+
+        
+    }
     async function classifyArray (keyIn) {
         
         let response = await fetch("https://token-list-api.solana.cloud/v1/list");
@@ -914,7 +922,7 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
                                                     <td class=" min-w-[4rem] text-center text-xs">ready</td>
                                                 {/if}
                                                 
-                                                <td class=" min-w-[4rem] text-right text-xs"><button class="btn btn-ghost btn-xs min-w-[2rem]" on:click={()=> ($keyList.splice(i,1), $keyList=$keyList)}>
+                                                <td class=" min-w-[4rem] text-right text-xs"><button class="btn btn-ghost btn-xs min-w-[2rem]" on:click={()=> (removeKeyHandler(i))}>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-4 h-4 stroke-primary">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                                       </svg>
