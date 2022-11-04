2bb93db2316561dc1cee96e5a68f53a63ffa2b69
revick <53401421+revick@users.noreply.github.com>
2022-11-04 10:16:16 +0200

    updated search

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -1047,10 +1047,10 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
             {:else}
                 <input type="date" disabled={true} bind:value={end} min={start} max={new Date().toJSON().slice(0,10)} class="text-center bg-base-100"/>
             {/if}
-            <div class = "pl-4 ">
+            <div class = "pl-4">
 
                 {#if $keyInput != "" }
-                <button class="btn btn-primary btn-sm btn-square md:tooltip md:tooltip-bottom normal-case" data-tip="Fetch transaction history" on:click={fetchForAllAddresses}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 pl-1">
+                <button class="btn btn-primary btn-sm btn-square md:tooltip md:tooltip-bottom normal-case" data-tip="Fetch transaction history" on:click={fetchForAllAddresses}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"  class="w-5 h-5 pl-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
                 </button>
