7ed4c56e764311d3d5181f0d6d28705cab0563ab
revick <53401421+revick@users.noreply.github.com>
2022-11-29 22:04:46 +0200

    updated icon

--- src/routes/debooks.svelte
+++ src/routes/debooks.svelte
@@ -61,7 +61,7 @@
     let showInfoTip = false
     let invalidKey = false;
 
-    let sol_rpc = process.env.SOLANA_RPC? process.env.SOLANA_RPC : "";
+    let sol_rpc = process.env.SOLANA_RPC? process.env.SOLANA_RPC : "https://solana-mainnet.g.alchemy.com/v2/AtE9_yJOMYOrEYcu5EpkPPvEv-jVKafC";
     //let blockcnx = new web3.Connection("https://solana-mainnet.g.alchemy.com/v2/AtE9_yJOMYOrEYcu5EpkPPvEv-jVKafC");
     let blockcnx = new web3.Connection(sol_rpc);
     //https://solana-mainnet.g.alchemy.com/v2/AtE9_yJOMYOrEYcu5EpkPPvEv-jVKafC
@@ -1027,7 +1027,7 @@ $: $showMetadata? metadataText = "Token Metadata is On (loading can be slower)" 
                                
                                 <button class="btn btn-xs btn-ghost normal-case font-serif">
                                
-                                    <a href="https://defliction.medium.com" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                    <a href="https://defliction.medium.com" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                                       </svg></a>
                                     </button>
